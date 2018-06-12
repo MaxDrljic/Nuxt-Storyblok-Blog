fb23517a55b54396de3563c852351ad9f5755be2
MaxDrljic <max_spree@hotmail.com>
2018-06-12 18:07:50 +0200

    Basic UI for the blog with dummy data

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section id="about-page">
+    <h1>Hi, this is my about page!</h1>
+    <p>My name is Max and I am 20 years old.</p>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
